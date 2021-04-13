b00b8cf879a0494baec7889349f44ccebe0cb8ce
Mr-Brown-95 <rodri89058@gmail.com>
2021-04-13 17:42:31 -0500

    actualizacion bd

--- CONSUVINO.sql
+++ CONSUVINO.sql
@@ -3,12 +3,12 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 10-04-2021 a las 03:33:11
+-- Tiempo de generación: 14-04-2021 a las 00:38:31
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
+        START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -29,28 +29,27 @@ SET time_zone = "+00:00";
 create  database CONSUVINO;
 use CONSUVINO;
 CREATE TABLE `producto` (
-                            `id` int(11) NOT NULL,
-                            `nombre` varchar(50) NOT NULL,
-                            `descripcion` varchar(100) NOT NULL,
-                            `categoria` varchar(50) NOT NULL,
-                            `existencia` int(11) DEFAULT 0,
-                            `fechaEntrada` date DEFAULT NULL,
-                            `cantidadEntrada` int(11) DEFAULT NULL,
-                            `fechaSalida` varchar(50) DEFAULT NULL,
-                            `cantidadSalida` int(11) DEFAULT NULL,
-                            `IdEmpRecibo` int(11) DEFAULT NULL,
-                            `IdEmpSurte` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    `id` int(11) NOT NULL,
+    `nombre` varchar(50) NOT NULL,
+    `descripcion` varchar(100) NOT NULL,
+    `categoria` varchar(50) NOT NULL,
+    `existencia` int(11) DEFAULT 0,
+    `fechaEntrada` date DEFAULT NULL,
+    `cantidadEntrada` int(11) DEFAULT NULL,
+    `fechaSalida` varchar(50) DEFAULT NULL,
+    `cantidadSalida` int(11) DEFAULT NULL,
+    `IdEmpRecibo` int(11) DEFAULT NULL,
+    `IdEmpSurte` int(11) DEFAULT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `producto`
 --
 
 INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `categoria`, `existencia`, `fechaEntrada`, `cantidadEntrada`, `fechaSalida`, `cantidadSalida`, `IdEmpRecibo`, `IdEmpSurte`) VALUES
-(1, 'uno', 'uno', 'uno', 20, '2021-04-09', 100, NULL, NULL, 1, NULL),
-(2, 'dos', 'dos\r\n', 'dos', 1, '2021-04-09', 99999, NULL, NULL, 1, NULL),
-(58, 'Tres', 'Tres', 'Tres', 0, NULL, NULL, NULL, NULL, NULL, NULL),
-(59, 'Cuatro', 'Cuatro', 'Cuatro', 0, NULL, NULL, NULL, NULL, NULL, NULL);
+    (1, 'Don Julio Blanco', 'TEQUILA DON JULIO BLANCO 700ML', 'Blanco, Destilados, Tequila', 0, '2021-04-13', 100, '2021-04-13', 50, 1, NULL),
+    (89, 'Ron Barcelo Imperial', 'RON BARCELO IMPERIAL 750ML', 'Ron', 50, '2021-04-13', 100, '2021-04-13', 50, NULL, NULL),
+    (90, 'Whisky Jack Daniel\'s', 'Whisky Jack Daniel\'s Single Barrel 700 ml.', 'whisky escocés', 50, '2021-04-13', 50, '2021-04-13', 50, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -59,22 +58,22 @@ INSERT INTO `producto` (`id`, `nombre`, `descripcion`, `categoria`, `existencia`
 --
 
 CREATE TABLE `serie` (
-                         `id` int(11) NOT NULL,
-                         `nombreSerie` varchar(50) NOT NULL,
-                         `sinopsis` varchar(300) NOT NULL,
-                         `actores` varchar(150) NOT NULL,
-                         `url` varchar(100) DEFAULT NULL,
-                         `fechaEstreno` varchar(50) NOT NULL,
-                         `activo` tinyint(4) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    `id` int(11) NOT NULL,
+    `nombreSerie` varchar(50) NOT NULL,
+    `sinopsis` varchar(300) NOT NULL,
+    `actores` varchar(150) NOT NULL,
+    `url` varchar(100) DEFAULT NULL,
+    `fechaEstreno` varchar(50) NOT NULL,
+    `activo` tinyint(4) NOT NULL DEFAULT 1
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `serie`
 --
 
 INSERT INTO `serie` (`id`, `nombreSerie`, `sinopsis`, `actores`, `url`, `fechaEstreno`, `activo`) VALUES
-(1, 'Filmed Before a Live Studio Audience', 'La pareja de recién casados Wanda y Visión se mudan a la ciudad de Westview durante lo que parece ser la década de 1950. Intentan mezclarse, a pesar de que Visión es un androide y Wanda tiene habilidades telequinéticas.', 'Elizabeth Olsen, Paul bettany, Kat Dennings', 'https://drive.google.com/file/d/1ZgSBa0r2l5oN12MG3SLsLmDT3z_Fmdo0/view?usp=sharing', '2021-01-15', 1),
-(2, 'Don\'t Touch That Dial', 'Durante lo que parece ser la década de 1960, Wanda y Visión oyen extraños ruidos fuera de su casa, que parecen ser causados por una rama de árbol en la ventana. Preparan su acto de magia para un espectáculo de talentos del vecindario.', 'Elizabeth Olsen, Paul bettany, Kat Dennings', 'https://drive.google.com/file/d/1Zh6x_d9cH9A0qInvrre92P_PFj5cqT5v/view?usp=sharing', '2021-01-15', 1),
+    (1, 'Filmed Before a Live Studio Audience', 'La pareja de recién casados Wanda y Visión se mudan a la ciudad de Westview durante lo que parece ser la década de 1950. Intentan mezclarse, a pesar de que Visión es un androide y Wanda tiene habilidades telequinéticas.', 'Elizabeth Olsen, Paul bettany, Kat Dennings', 'https://drive.google.com/file/d/1ZgSBa0r2l5oN12MG3SLsLmDT3z_Fmdo0/view?usp=sharing', '2021-01-15', 1),
+    (2, 'Don\'t Touch That Dial', 'Durante lo que parece ser la década de 1960, Wanda y Visión oyen extraños ruidos fuera de su casa, que parecen ser causados por una rama de árbol en la ventana. Preparan su acto de magia para un espectáculo de talentos del vecindario.', 'Elizabeth Olsen, Paul bettany, Kat Dennings', 'https://drive.google.com/file/d/1Zh6x_d9cH9A0qInvrre92P_PFj5cqT5v/view?usp=sharing', '2021-01-15', 1),
 (3, 'Now in Color', '\r\nWanda se prepara para un parto acelerado con Visión, pero el embarazo daña sus poderes. ', 'Elizabeth Olsen, Paul bettany, Kat Dennings', 'https://drive.google.com/file/d/1ZcR9zAXE9DDI39WksEC5WEGrA08OxxGp/view?usp=sharing', '2021-01-22', 1),
 (4, 'We Interrupt This Program', 'Monica Rambeau, encargada de una tarea especial con respecto a las armas sensibles, desaparece. ', 'Elizabeth Olsen, Paul bettany, Kat Dennings', 'https://drive.google.com/file/d/1ZzuKqdst3CLoHNRFufkSpLSw4ARFCHyG/view?usp=sharing', '2021-01-29', 1),
 (5, 'On a Very Special Episode...', '\r\nWanda aborda las preocupaciones de Vision cuando comienza a sospechar del extraño comportamiento de los vecinos. ', 'Elizabeth Olsen, Paul bettany, Kat Dennings', 'https://drive.google.com/file/d/1_0qepIAvmiHTc8V5gu6z4GUAX1GmamKi/view?usp=sharing', '2021-02-05', 1),
@@ -102,8 +101,11 @@ CREATE TABLE `ubicacion` (
 --
 
 INSERT INTO `ubicacion` (`id`, `pasillo`, `rack`, `nivel`, `idEmpleado`) VALUES
-(19, '3', 'rack3', 3, 1),
-(21, '10', '10', 10, 10);
+(19, '1', 'rack1', 1, 1),
+(21, '10', '10', 10, 10),
+(22, 'm', 'm', 566, 5),
+(23, 'Pasillo66', 'rack66', 66, 6),
+(24, 'b', 'b', 199, 1);
 
 -- --------------------------------------------------------
 
@@ -126,16 +128,9 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombreUsuario`, `usuario`, `password`, `foto`, `privilegios`, `activo`) VALUES
-(5, 'Rodrigo Hernandez', 'asdfAdministrador', '$2y$10$5sPPaQGpis4XeBUfgoU0s.ZsasE25Y9jcj0/VErEfpRK/mfQa0Mwi', 'img/1.png', 'Administrador', 1),
-(110, 'a', 'a', '$2y$10$cAY3ND0vidRjb8HCf1i61eMtxnb8MnRBtHwNwxF3d6gqVH5xFd4bS', '', 'General', 1),
-(111, 'prueba10', 'prueba10@prueba10.com', '$2y$10$cp2fdIvkZ87EK6aR4W9CDexFgsUDOTosJ1qzJMA6dq3AX7OPI5Qku', '', 'General', 1),
-(112, 'aa', 'asdf', '$2y$10$bZpgKgXHoD/mohtyVZId4OUyrPBEI64pw3rhVFAmBkGBZe.mArx5u', '', 'General', 0),
-(113, 'aaa', '%', '$2y$10$vV0eY/reGXHwmwsW2y0mf.RFUuwPYmFcloBSX3grfveABTj2ZbNRu', '', 'General', 0),
-(114, 'aa', 'aa', '$2y$10$y0M8MDKGPUGlzMMjr54UQ.xpRtLRJq1/T9yhE.7dbVJU351dBsMbO', '', 'a', 1),
-(115, 'dos dos', 'dos@.com', '$2y$10$7hV0jFEifqx0nogtvZR84uFWwN7itH14EB8KDK6JESt1iVG0YcSkG', 'img/1.png', 'General', 0),
-(116, 'khb', 'ñkn', '$2y$10$y6BXlb89/rhRr4IYlVNH1.cJioGz4hb8o2eiU6mCIkDj89VKvpTvy', '', 'General', 1),
-(117, 'prueba10 prueba10', 'asdfAdministrador', '$2y$10$qfvslIMBYSknZKMupwWhhuvMxxX.b5yJjJJ7bb8IEXCc6E/O02jCa', '', 'General', 1),
-(118, 'mensajero', 'mensajero@mensajero.com', '$2y$10$6spZ7vDn5BSjxBzgn7ZbcOdqXw94mWlRoKCzJNCDyyCX4ndfQS68G', '', 'General', 1);
+(124, 'Patricia Franco', 'Patricia.Franco@gmail.com', '$2y$10$TpnGKJrIH3JJazSvOJVkr.fJLxq7BcSxRD2Su1rM.5pD4.pWpWEkm', 'img/20210413_155749.jpg', 'General', 1),
+(126, 'Brayan Aguayo', 'Alan.Brayan@gmail.com', '$2y$10$Yc9ss4oanC1gyxBPdYqvTuLSqF5nT5/9OYhylhMmiZQQfxpFdRB7G', 'img/20210413_155750.jpeg', 'General', 1),
+(127, 'Rodrigo Hernandez', 'Rodrigo.Hernandez@gmail.com', '$2y$10$5BmmWlPvRV1tFLsXuTWAOu0RaheUt9grpebepATDQqeac84muX3Qu', 'img/IMG-20210413-WA0005.jpg', 'Administrador', 1);
 
 --
 -- Índices para tablas volcadas
@@ -173,7 +168,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
 
 --
 -- AUTO_INCREMENT de la tabla `serie`
@@ -185,13 +180,13 @@ ALTER TABLE `serie`
 -- AUTO_INCREMENT de la tabla `ubicacion`
 --
 ALTER TABLE `ubicacion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=128;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
